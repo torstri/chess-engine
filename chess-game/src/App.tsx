9241d3bc71b812b8270886d36e8b59a90584172b
HenrikCarlGustavBertilAkesson <henrik.akesson19@gmail.com>
2025-01-11 19:06:43 +0100

    fix random move is illegal

--- chess-game/src/App.tsx
+++ chess-game/src/App.tsx
@@ -11,11 +11,9 @@ type ModifyFunction = (game: Chess) => void;
 function App(): JSX.Element {
   const [game, setGame] = useState<Chess>(new Chess());
   const [gameFEN, setGameFEN] = useState<string>(game.fen());
-  const [possibleMoves, setPossibleMoves] = useState<string[]>(game.moves());
 
   useEffect(() => {
     setGameFEN(game.fen());
-    setPossibleMoves(game.moves());
   }, [game]);
 
   // Function to safely mutate the game state
@@ -29,16 +27,17 @@ function App(): JSX.Element {
 
   // Function to make a random move for the computer
   function makeRandomMove(): void {
-    if (game.isGameOver() || game.isDraw() || possibleMoves.length === 0)
+    if (game.isGameOver() || game.isDraw())
       return;
 
-    const randomIndex = Math.floor(Math.random() * possibleMoves.length); // Pick a random move from possible moves
-    const randomMove = possibleMoves[randomIndex];
-
+    
     safeGameMutate((game) => {
-      game.move(game.moves()[0]); // Apply the move
+      const randomMoves = game.moves();
+      if(randomMoves.length == 0) return;
+
+      const randomIndex = Math.floor(Math.random() * randomMoves.length); 
+      game.move(randomMoves[randomIndex]);
       fenToBoardRepresenation(game.fen());
-      console.log("Lastly: ", game);
     });
   }
 
@@ -51,9 +50,8 @@ function App(): JSX.Element {
 
     if (move === null) return false; // Illegal move
 
-    // setGameFEN(game.fen()); // Update the FEN after a valid move
     setTimeout(() => {
-      makeRandomMove(); // Make a random move after a valid move
+      makeRandomMove();
     }, 200);
 
     return true; // Valid move
