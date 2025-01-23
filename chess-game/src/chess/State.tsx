98b50d8ec80991526f2467166b9e4097f9f9a7f0
HenrikCarlGustavBertilAkesson <henrik.akesson19@gmail.com>
2025-01-23 20:03:52 +0100

    piece square tables

--- chess-game/src/chess/State.tsx
+++ chess-game/src/chess/State.tsx
@@ -1,10 +1,4 @@
 import { Chess, Move } from "chess.js";
-import { pieceValue } from "./chessAI";
-
-enum stateBias {
-  castled = 0.5,
-  inCheck = 0.5,
-}
 
 export class State {
   fen: string;
@@ -13,24 +7,6 @@ export class State {
   constructor(fen: string, score?: number) {
     this.fen = fen;
     this.totalScore = score ? score : 0;
-  }
-
-  addStateScoreBias(game: Chess, move: Move, player: string): number {
-    let score = 0;
-
-    if (game.isCheck())
-      score += move.color == player ? stateBias.inCheck : -stateBias.inCheck;
-
-    if(move.san.includes('O-O') || move.san.includes('O-O-O')) 
-      score += move.color == player ? stateBias.castled : -stateBias.castled;
-
-    if (move.captured)
-      score +=
-        move.color == player
-          ? pieceValue[move.captured] / pieceValue['q']
-          : -(pieceValue[move.captured] / pieceValue['q']);
-
-    return score;
   }
 
   possibleMoves(): Move[] {
---
+++ chess-game/src/chess/utils/Constants.tsx
@@ -0,0 +1,14 @@
+export const C = 2;
+export const MAXDEPTH = 5;
+export const duration = 500;
+export const totPieceValue = 39;
+
+export const pieceValue = {
+  p: 10,
+  n: 30,
+  b: 30,
+  r: 50,
+  q: 90,
+  k: 999,
+  "": 0,
+};
