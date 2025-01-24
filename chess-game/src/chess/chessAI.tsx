924f3df051a6c3e091838c18658f1016639e72f3
Tor Strimbold <tstrimbold@gmail.com>
2025-01-24 23:08:29 +0100

    Integration

--- chess-game/src/chess/chessAI.tsx
+++ chess-game/src/chess/chessAI.tsx
@@ -1,8 +1,21 @@
-import { Chess, Move, PieceSymbol} from "chess.js";
+import { Chess, Move, PieceSymbol } from "chess.js";
 import { Node } from "./Node";
 import { State } from "./State";
-import { C, MAXDEPTH, ALLOWED_DURATION } from "./utils/Constants";
-import { evaluateTerminalState, sumPieceSquareEvaluation, evaluateMaterial, evaluateMobility, evaluateThreats } from "./utils/Evaluation";
+import {
+  C,
+  MAXDEPTH,
+  ALLOWED_DURATION,
+  MOBILITY_WEIGHT,
+  MATERIAL_WEIGHT,
+} from "./utils/Constants";
+import {
+  evaluateTerminalState,
+  evaluateState,
+  evaluateMove,
+  evaluateMobility,
+  materialEvaluation,
+  mobiltyEvaluation,
+} from "./utils/Evaluation";
 import { Player } from "./utils/Types";
 
 // Statistics
@@ -12,7 +25,7 @@ let expansionCount = 0;
 let selectionTime = 0;
 let rolloutTime = 0;
 let expansionTime = 0;
-
+let print = true;
 
 export class ChessAI {
   player: string;
@@ -26,7 +39,6 @@ export class ChessAI {
 
   // Monte Carlo Tree Search
   makeMove(game: Chess): Move {
-
     this.root = new Node(new State(game.fen()), this.player, 0);
 
     const startTime = Date.now();
@@ -54,7 +66,7 @@ export class ChessAI {
       }
     }
 
-    this.consoleLogs(game, this.player);
+    this.printStatistics(game);
 
     rolloutCount = 0;
     expansionCount = 0;
@@ -64,26 +76,23 @@ export class ChessAI {
     return this.getBestMove(this.root);
   }
 
-  getBestMove(root: Node, verbose?: boolean): Move {
-
+  getBestMove(root: Node): Move {
     let move: Move | undefined;
     let maxScore = -Infinity;
-    let bestChild: Node | undefined;
     root.children.forEach((ch: Node) => {
       if (ch.state.totalScore > maxScore) {
         move = ch.move;
         maxScore = ch.state.totalScore;
-        bestChild = ch;
       }
     });
 
     if (!move) throw new Error("No move found");
 
-    if(verbose) {
+    if (print) {
       console.log("------------ New Move --------------");
       const visits = this.root?.visits;
       const score = this.root?.state.totalScore;
-      if (visits && score && verbose) {
+      if (visits && score && print) {
         console.log("Best move found: ", move);
         console.log(
           "With evaluated position : ",
@@ -135,15 +144,17 @@ export class ChessAI {
     }
   }
 
-  rollout(game: Chess, depth: number): number {
+  rollout(game: Chess, depth: number, move?: Move): number {
     if (game.isGameOver()) return evaluateTerminalState(game, this.player);
 
     if (depth > MAXDEPTH) {
-      return sumPieceSquareEvaluation(game, this.player);
+      let score = evaluateState(game, this.player);
+      score += move ? evaluateMove(game, move, this.player) : 0;
+      return score;
     }
 
     const randomIndex = Math.floor(Math.random() * game.moves().length);
-    const randomMove = game.moves()[randomIndex];
+    const randomMove = game.moves({ verbose: true })[randomIndex];
 
     try {
       game.move(randomMove);
@@ -152,11 +163,10 @@ export class ChessAI {
       throw new Error("INVALID MOVE");
     }
 
-    return this.rollout(game, depth + 1);
+    return this.rollout(game, depth + 1, randomMove);
   }
 
   ucb1(score: number, N: number, n: number): number {
-
     if (n == 0 || N == 0) return Infinity;
 
     return score / n + C * Math.sqrt(Math.log(N) / n);
@@ -207,36 +217,16 @@ export class ChessAI {
       console.log(`Row ${rowIndex + 1}: ${row}`);
     });
   }
-  
-  consoleLogs(game: Chess, color: string) {
-    const whiteMobility = evaluateMobility(game, this.player, Player.White);
-    const blackMobility = evaluateMobility(game, this.player, Player.Black);
 
-    // Normalize
-    const mobilityScore =
-      color === Player.White
-        ? whiteMobility - blackMobility
-        : blackMobility - whiteMobility;
-
-    const whiteMaterial = evaluateMaterial(game, Player.White);
-    const blackMaterial = evaluateMaterial(game, Player.Black);
-
-    const materialScore =
-      color === Player.White
-        ? whiteMaterial - blackMaterial
-        : blackMaterial - whiteMaterial;
-
-    const whiteThreats = evaluateThreats(game, Player.White);
-    const blackThreats = evaluateThreats(game, Player.Black);
-    const alternatvetTthreatEvaluation =
-      color === Player.White
-        ? blackThreats - whiteThreats
-        : whiteThreats - blackThreats;
-    const threatEvaluation = evaluateThreats(game, color);
-    console.log("Mobility Score: ", 0.1 * mobilityScore);
-    console.log("Material Score: ", materialScore);
-    console.log("Threat Score: ", -threatEvaluation);
-    console.log("Alt Threat Score: ", alternatvetTthreatEvaluation);
+  printStatistics(game: Chess) {
+    if (print) {
+      const opponent = this.player === "w" ? "b" : "w";
+      const mobilityScore = mobiltyEvaluation(game, this.player, opponent);
+      const materialScore = materialEvaluation(game, this.player);
+      console.log("Playing as: ", this.player);
+      console.log("Mobility Score: ", MOBILITY_WEIGHT * mobilityScore);
+      console.log("Material Score: ", MATERIAL_WEIGHT * materialScore);
+    }
 
     console.log(
       "Rollouts: ",
--- chess-game/src/chess/utils/Evaluation.tsx
+++ chess-game/src/chess/utils/Evaluation.tsx
@@ -1,197 +1,210 @@
-import { Chess, Move, PieceSymbol } from "chess.js";
+import { Chess, Move, PieceSymbol, validateFen } from "chess.js";
 import { stateBias, Player } from "./Types";
-import { pieceValue } from "./Constants";
-import { PSQT, getSquareInTable } from "./PSQT";
+import {
+  pieceValue,
+  END_GAME_PIECE_AMOUNT,
+  MOBILITY_WEIGHT,
+  MATERIAL_WEIGHT,
+} from "./Constants";
+import { PSQT_MAP, getSquareInTable } from "./PSQT";
 
+// Evaluates if a game is end game or not
+export function isEndGame(game: Chess): boolean {
+  const fenString = game.fen();
+  const boardState = fenString.split(" ")[0];
+
+  const pieceCount = boardState.split("").reduce((count, char) => {
+    if (/[prnbqkPRNBQK]/.test(char)) {
+      return count + 1;
+    }
+    return count;
+  }, 0);
+
+  return pieceCount < END_GAME_PIECE_AMOUNT;
+}
+
+export function evaluateMove(game: Chess, move: Move, player: string): number {
+  let moveScore = 0;
+
+  if (game.isCheck())
+    moveScore += move.color === player ? stateBias.inCheck : -stateBias.inCheck;
+
+  if (move.san.includes("O-O") || move.san.includes("O-O-O"))
+    moveScore += move.color === player ? stateBias.castled : -stateBias.castled;
+
+  // Incentive for capturing / not being captured
+  if (move.captured)
+    moveScore +=
+      move.color == player
+        ? pieceValue[move.captured] * 10
+        : -10 * pieceValue[move.captured];
+
+  return moveScore;
+}
+
+// Evaluates a terminal state
 export function evaluateTerminalState(game: Chess, player: String): number {
-    let win;
+  let outCome;
 
-    if (game.isCheckmate()) {
-      win = game.turn() == player ? -stateBias.checkMate : stateBias.checkMate;
-    } else {
-      win = stateBias.draw;
+  if (game.isCheckmate()) {
+    outCome =
+      game.turn() === player ? -stateBias.checkMate : stateBias.checkMate;
+  } else {
+    outCome = stateBias.draw;
+  }
+
+  return outCome;
+}
+
+// Evaluates a non-terminal state
+export function evaluateState(game: Chess, player: string): number {
+  let opponent = player === "w" ? "b" : "w";
+
+  // const mobilityScore = mobiltyEvaluation(game, player, opponent);
+  const materialScore = materialEvaluation(game, player);
+  // const threatScore = threatEvaluation(game, player);
+  return MATERIAL_WEIGHT * materialScore;
+
+  // return MOBILITY_WEIGHT * mobilityScore + MATERIAL_WEIGHT * materialScore;
+}
+
+export function mobiltyEvaluation(
+  game: Chess,
+  player: string,
+  opponent: string
+): number {
+  // https://www.chessprogramming.org/Evaluation#General_Aspects
+  let playerMobility = 0;
+  let opponentMobility = 0;
+
+  let gameFen = game.fen();
+  let fenParts = gameFen.split(" ");
+  let color = game.turn() === player ? opponent : player;
+  fenParts[1] = color;
+  let newFen = fenParts.join(" ");
+
+  let gameCopy = new Chess();
+  if (validateFen(newFen).ok) {
+    gameCopy = new Chess(newFen);
+  } else {
+    gameCopy = new Chess(game.fen());
+    const randomIndex = Math.floor(Math.random() * game.moves().length);
+    const randomMove = gameCopy.moves()[randomIndex];
+    gameCopy.move(randomMove);
+  }
+
+  if (game.turn() === player) {
+    playerMobility = evaluateMobility(game, player);
+    opponentMobility = evaluateMobility(gameCopy, opponent);
+  } else {
+    playerMobility = evaluateMobility(gameCopy, player);
+    opponentMobility = evaluateMobility(game, opponent);
+  }
+
+  return playerMobility - opponentMobility;
+}
+
+export function valueOfNewPos(move: Move): number {
+  const from = move.from;
+  const to = move.to;
+  const piece = move.piece;
+  const psqt = PSQT_MAP[piece];
+
+  const squareFrom = getSquareInTable(from);
+  const squareTo = getSquareInTable(to);
+  const valueFrom = psqt ? psqt[squareFrom.rowIdx][squareFrom.colIdx] : 0;
+  const valueTo = psqt ? psqt[squareTo.rowIdx][squareTo.colIdx] : 0;
+
+  return psqt ? pieceValue[piece] * 0.1 * (valueTo - valueFrom) : 0;
+}
+
+export function valueOfSquare(piece: PieceSymbol, square: string): number {
+  const tableIndex = getSquareInTable(square);
+  const psqt = PSQT_MAP[piece];
+
+  return psqt
+    ? pieceValue[piece] + psqt[tableIndex.rowIdx][tableIndex.colIdx]
+    : 0;
+}
+
+export function evaluateMobility(game: Chess, color: string): number {
+  // https://www.chessprogramming.org/Evaluation#General_Aspects
+
+  // game.turn == color
+  const legalMovesMap: { [key: string]: number } = {};
+  let newPosVal = 0;
+  let defenseValue = 0;
+
+  game.moves({ verbose: true }).forEach((move) => {
+    // value[move.piece] * PSQT(move.to)
+    newPosVal += valueOfNewPos(move);
+    if (move.captured) {
+      // Value of enemy * PSQT(enemy) + Value of piece * PSQT(move.to) - Value of piece * PSQT(move.from)
+      newPosVal += valueOfSquare(move.captured, move.to);
     }
 
-    return win;
-  }
+    // Save all squares we can move to with a legal move
+    legalMovesMap[move.to] = legalMovesMap[move.to]
+      ? legalMovesMap[move.to]++
+      : 1;
+  });
 
-  export function evaluateState(game: Chess, player: string, move: Move): number {
-    let score = 0;
-
-    if (game.isCheck())
-      score += move.color == player ? stateBias.inCheck : -stateBias.inCheck;
-
-    if (move.san.includes('O-O') || move.san.includes('O-O-O')) 
-      score += move.color == player ? stateBias.castled : -stateBias.castled;
-
-    if (move.captured)
-      score +=
-        move.color == player
-          ? pieceValue[move.captured] * 10
-          : -10 * (pieceValue[move.captured]);
-
-    if (move.color == player) {
-      const square = getSquareInTable(move);
-      score += pieceSquareEvaluation(square.rowIdx, square.colIdx, move.piece);
-    }
-
-    const mobilityScore = mobiltyEvaluation(game, player);
-    const materialScore = materialEvaluation(game, player);
-    const threatScore = threatEvaluation(game, player);
-
-    return 0.1 * mobilityScore + 2 * materialScore + 0.5 * threatScore + score;
-  }
-
-  export function mobiltyEvaluation(game: Chess, color: string): number {
-    const whiteMobility = evaluateMobility(game, color, Player.White);
-    const blackMobility = evaluateMobility(game, color, Player.Black);
-
-    const mobilityScore =
-      color === Player.White
-        ? whiteMobility - blackMobility
-        : blackMobility - whiteMobility;
-
-    return mobilityScore;
-  }
-
-  export function evaluateMobility(game: Chess, player: string, color: string): number {
-
-    const attackedSquaresMap: { [key: string]: number } = {};
-
-    let attackedSquares = 0; // Keeps track of our legal moves
-
-    if (game.turn() !== player) {
-      let gameFen = game.fen();
-      let fenParts = gameFen.split(" ");
-
-      fenParts[1] = color; // Set the turn to the desired color
-
-      let newFen = fenParts.join(" ");
-
-      let newGame = new Chess();
-
-      try {
-        newGame.load(newFen);
-      } catch (error) {
-        newGame = game;
-        const randomIndex = Math.floor(Math.random() * game.moves().length);
-        const randomMove = newGame.moves()[randomIndex];
-        newGame.move(randomMove);
+  game.board().forEach((row) => {
+    row.forEach((square) => {
+      // We control the square
+      if (square?.type && color === square?.color) {
+        // We also attack the square, and it is not a legal move
+        if (
+          game.isAttacked(square.square, color) &&
+          !legalMovesMap[square.square]
+        ) {
+          defenseValue += valueOfSquare(square.type, square.square);
+        }
       }
-
-      newGame.moves({ verbose: true }).forEach((move) => {
-        attackedSquaresMap[move.to] = attackedSquaresMap[move.to]
-          ? 1
-          : attackedSquaresMap[move.to] + 1;
-        attackedSquares++;
-      });
-    } else {
-      game.moves({ verbose: true }).forEach((move) => {
-        attackedSquaresMap[move.to] = attackedSquaresMap[move.to]
-          ? 1
-          : attackedSquaresMap[move.to] + 1;
-        attackedSquares++;
-      });
-    }
-
-    let defendedSquares = 0;
-    game.board().forEach((row) => {
-      row.forEach((square) => {
-        if (square?.square && color === square.color) {
-          if (!attackedSquaresMap[square.square]) {
-            defendedSquares += game.isAttacked(square.square, square.color)
-              ? 1 * pieceValue[square.type]
-              : 0;
-          }
-        }
-      });
     });
+  });
 
-    return defendedSquares + attackedSquares;
-  }
+  return newPosVal + defenseValue;
+}
 
-  export function materialEvaluation(game: Chess, color: string): number {
-    const whiteMaterial = evaluateMaterial(game, Player.White);
-    const blackMaterial = evaluateMaterial(game, Player.Black);
-
-    const materialScore =
-      color === Player.White
-        ? whiteMaterial - blackMaterial
-        : blackMaterial - whiteMaterial;
-
-    return materialScore;
-  }
-
-  export function evaluateMaterial(game: Chess, color?: string): number {
-    let score = 0;
-    game.board().forEach((row) => {
-      row.forEach((square) => {
-
-        if ((color && square && square.color === color) || (!color && square)) {
-          if (square.type !== "k") {
-            score += pieceValue[square.type];
-          }
-        }
-      });
+export function materialEvaluation(game: Chess, player: string): number {
+  let playerScore = 0;
+  let opponentScore = 0;
+  game.board().forEach((row) => {
+    row.forEach((square) => {
+      if (square && square.color == player) {
+        playerScore += valueOfSquare(square.type, square.square);
+      } else if (square) {
+        opponentScore -= valueOfSquare(square.type, square.square);
+      }
     });
+  });
 
-    return score;
-  }
+  return playerScore - opponentScore;
+}
 
+// export function threatEvaluation(game: Chess, color: string): number {
+//   const whiteThreats = evaluateThreats(game, Player.White);
+//   const blackThreats = evaluateThreats(game, Player.Black);
+//   const threatEvaluation =
+//     color === Player.White
+//       ? blackThreats - whiteThreats
+//       : whiteThreats - blackThreats;
 
-  export function threatEvaluation(game: Chess, color: string): number {
-    const whiteThreats = evaluateThreats(game, Player.White);
-    const blackThreats = evaluateThreats(game, Player.Black);
-    const threatEvaluation =
-      color === Player.White
-        ? blackThreats - whiteThreats
-        : whiteThreats - blackThreats;
+//   return threatEvaluation;
+// }
 
-    return threatEvaluation;
-  }
-
-  export function evaluateThreats(game: Chess, color: string): number {
-      let threatenedSquares = 0;
-      let enemyColor = color === Player.White ? Player.Black : Player.White;
-      game.board().forEach((row) => {
-        row.forEach((square) => {
-          if (square?.color === color) {
-            if (game.isAttacked(square.square, enemyColor)) {
-              threatenedSquares += pieceValue[square.type];
-            }
-          }
-        });
-      });
-      return threatenedSquares;
-    }
-  
-  export function sumPieceSquareEvaluation(game: Chess, player: string): number {
-    let score = 0;
-    game.board().forEach((row, rowIdx) => {
-      row.forEach((square, colIdx) => {
-        if (square) { 
-
-          if(square.color == player) 
-            score += pieceSquareEvaluation(rowIdx, colIdx, square.type);
-          // else
-          //   score -= pieceSquareEvaluation(rowIdx, colIdx, square.type);
-
-        }
-      });
-    });
-
-    return score;
-  }
-
-  export function pieceSquareEvaluation(rowIdx: number, colIdx: number, piece: PieceSymbol): number {
-
-    const PSQT_MAP: { [key in PieceSymbol]?: number[][] } = {
-      p: PSQT.PAWN,   
-      b: PSQT.BISHOP, 
-      n: PSQT.KNIGHT, 
-      r: PSQT.ROOK,   
-      q: PSQT.QUEEN,  
-    };
-  
-    return PSQT_MAP[piece] ? PSQT_MAP[piece][rowIdx][colIdx] : 0;
-  }
+// export function evaluateThreats(game: Chess, color: string): number {
+//   let threatenedSquares = 0;
+//   let enemyColor = color === Player.White ? Player.Black : Player.White;
+//   game.board().forEach((row) => {
+//     row.forEach((square) => {
+//       if (square?.color === color) {
+//         if (game.isAttacked(square.square, enemyColor)) {
+//           threatenedSquares += pieceValue[square.type];
+//         }
+//       }
+//     });
+//   });
+//   return threatenedSquares;
+// }
